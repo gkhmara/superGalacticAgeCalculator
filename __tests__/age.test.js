22d90990c49816ea6942cd2eb8769f7117f393a9
Greg Khmara <gkhmara@gmail.com>
2022-03-12 15:17:58 -0800

    update first test

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -3,5 +3,7 @@ import Age from '../src/js/age.js';
 describe('Age', () => {
   test('should return age on Mercury', () => {
     let person1 = new Age("mercury" , 39);
+    expect(person1.ageConverter()).toEqual(162.5);
   });
 });
+
