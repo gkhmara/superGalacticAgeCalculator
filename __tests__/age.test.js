809f042337b28df7ee62cb2f341d3d7a7bf6e3d1
Greg Khmara <gkhmara@gmail.com>
2022-03-12 15:30:12 -0800

    add test three

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -5,9 +5,17 @@ describe('Age', () => {
     let person1 = new Age("mercury", 39);
     expect(person1.ageConverter()).toEqual(163);
   });
+
+  test('should return age on Venus', () => {
+    let person1 = new Age("venus", 39);
+    expect(person1.ageConverter()).toEqual(63);
+  })
+
+  test('should return age on Mars', () => {
+    let person1 = new Age("Mars", 39);
+    expect(person1.ageConverter()).toEqual(63);
+  })
+
 });
 
-test('should return age on Venus', () => {
-  let person1 = new Age("venus", 39);
-  expect(person1.ageConverter()).toEqual(63);
-})
+
