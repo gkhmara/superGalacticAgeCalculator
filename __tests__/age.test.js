6bd9e5a8b11384fffb5de668f75fb31b7d658ebc
Greg Khmara <gkhmara@gmail.com>
2022-03-12 14:53:50 -0800

    add files to project

---
+++ __tests__/age.test.js
@@ -0,0 +1,7 @@
+import Age from '../src/js/age.js';
+
+describe('Age', () => {
+  test('should return age on Mercury', () => {
+    let person1 = new Age("mercury" , 39);
+  });
+});
---
+++ src/js/age.js
@@ -0,0 +1,10 @@
+export default class Age {
+  constructor(planet, age) {
+    this.planet = planet;
+    this.age = age;
+    this.mercury = .24;
+    this.venus = .62;
+    this.mars = 1.88;
+    this.jupiter = 11.86;
+  }
+}
