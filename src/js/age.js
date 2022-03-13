1074b0624618ed9d1cf3f7fc16d24fbe9831defe
Greg Khmara <gkhmara@gmail.com>
2022-03-12 16:36:42 -0800

    update second function

--- src/js/age.js
+++ src/js/age.js
@@ -27,6 +27,12 @@ export default class Age {
       } else {
         return (age - (Math.round(this.age / this.mercury)));
       }
+    } else if (this.planet.toUpperCase() === "VENUS") {
+      if(age- (Math.round(this.age / this.venus)) < 0) {
+        return((age - (Math.round(this.age / this.venus))) * -1);
+      } else {
+        return (age - (Math.round(this.age / this.venus)));
+      }
     }
   }
 }
